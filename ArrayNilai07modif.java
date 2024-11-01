e4e36fd3e88722e750e88c621b165d6d3ed95746
dyhptri <hernandar0606@gmail.com>
2024-11-01 08:13:28 +0700

    modif2

---
+++ ArrayNilai07modif.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ArrayNilai07modif {
+    public static void main(String[] args) {
+        int[] nilaiAkhir = new int[10];
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            System.out.print("Masukkan nilai akhir ke- "+i+": ");
+            nilaiAkhir[i] = sc.nextInt();
+        }
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            if (nilaiAkhir[i] > 70) {
+                System.out.println("Mahasiswa ke- "+i+" lulus!");
+            } 
+        }
+    }
+}
